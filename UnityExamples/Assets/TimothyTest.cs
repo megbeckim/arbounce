9b5f3b6ecbee1071730b5e95bae7c8a6b7f46994
megbeckim <timothy@Timothys-MacBook-Air.local>
2015-08-10 19:40:51 -0400

    added dummy script to test with

---
+++ UnityExamples/Assets/TimothyTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimothyTest : MonoBehaviour {
+	public TangoPointCloud m_tangoPointCloud;
+	public Camera m_camera;
+	public Plane m_bounceSurface;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("got here");
+	}
+}
