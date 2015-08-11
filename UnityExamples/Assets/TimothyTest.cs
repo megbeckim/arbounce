9539bfcec78ad5565adc6df20dc3954c467d66fc
megbeckim <timothy@Timothys-MacBook-Air.local>
2015-08-10 21:30:28 -0400

    more experiments with point clouds!

--- UnityExamples/Assets/TimothyTest.cs
+++ UnityExamples/Assets/TimothyTest.cs
@@ -13,6 +13,25 @@ public class TimothyTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("got here");
+		Debug.Log ("pos " + m_camera.transform.position);
+		Debug.Log ("fwd " + m_camera.transform.forward);
+
+		Vector3 nearest = Vector3.zero;
+		Vector3 p1 = m_camera.transform.position;
+		Vector3 p2 = m_camera.transform.position + m_camera.transform.forward;
+		double minDistance = double.MaxValue;
+		int nearestIndex = 0;
+		for (int index = 0; index < m_tangoPointCloud.m_pointsCount; index++) {
+			Vector3 p0 = m_tangoPointCloud.m_points[index];
+			double distance = Vector3.Cross(p0 - p1, p0 - p2).magnitude;
+			if (distance < minDistance) {
+				minDistance = distance;
+				nearest = p0;
+				nearestIndex = index;
+			}
+		}
+
+		Debug.Log ("middle (" + nearestIndex + ") " + nearest);
+		m_bounceSurface.transform.position = nearest;
 	}
 }
